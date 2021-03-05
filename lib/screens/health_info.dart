cdd041105bcb17e1803ad903c37044c103276409
zenraiching <rickdavidsangalang@gmail.com>
2021-03-06 00:23:10 +0800

    add

---
+++ lib/screens/health_info.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HealthInfo extends StatefulWidget {
+  @override
+  _HealthInfoState createState() => _HealthInfoState();
+}
+
+class _HealthInfoState extends State<HealthInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
