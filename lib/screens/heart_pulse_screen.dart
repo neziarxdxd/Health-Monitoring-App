86e525dc619a6eaa56eb462729085e886b512859
zenraiching <rickdavidsangalang@gmail.com>
2021-04-21 20:08:31 +0800

    add

--- lib/screens/heart_pulse_screen.dart
+++ lib/screens/heart_pulse_screen.dart
@@ -32,7 +32,7 @@ class HeartPulseScreenView extends State<HeartPulse>
   void initState() {
     super.initState();
     _animationController =
-        AnimationController(duration: Duration(milliseconds: 500), vsync: this);
+        AnimationController(duration: Duration(milliseconds: 500));
     _animationController
       ..addListener(() {
         setState(() {
